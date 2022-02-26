cb88e9b979b90ab56ae085a09ef647ec482da7bc
Adam U.W. Myers <amyers14@uwyo.edu>
2022-02-26 07:53:12 -0700

    set up an example package

---
+++ homework/HW2.py
@@ -0,0 +1,5 @@
+def example_function():
+    print("Salut folks")
+
+if __name__ == "__main__":
+    example_function()
---
+++ tasks/week7/class14.py
@@ -0,0 +1,5 @@
+def example_function():
+    print("Howdy globe")
+
+if __name__ == "__main__":
+    example_function()
